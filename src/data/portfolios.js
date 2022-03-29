13d3ba41e1e8529e7e6024cf04920139b912bf13
Dharanikumar19 <dharanikumarmuthusamy@gmail.com>
2022-03-29 16:02:54 +0530

    img changes

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,9 +1,12 @@
-import project from '../img/portImages/project.png';
+import blog from '../img/portImages/blog.jpg';
+import shop from '../img/portImages/shop.jpg';
+import social from '../img/portImages/social.png';
+import zen from '../img/portImages/zen.png';
 const portfolios = [
     {
         id: 1,
         category: 'MERN',
-        image: project,
+        image: zen,
         link1: 'https://github.com/Dharanikumar19/ZenClass-Ticketing-System-Frontend',
         link2: 'https://github.com/Dharanikumar19/ZenClass-Ticketing-System-Backend',
         link3: 'https://zen-class-ticketing-system.netlify.app',
@@ -14,33 +17,34 @@ const portfolios = [
     {
         id: 2,
         category: 'MERN',
-        image: project,
+        image: shop,
         link1: 'https://github.com/Dharanikumar19/e-commerce-frontend',
         link2: 'https://github.com/Dharanikumar19/e-commerce-backend',
         link3: 'https://dk-e-commerce.netlify.app',
-        title: 'E-Commerce Shopping Website',
-        text: `e-commerce shopping website is a responsive full stack web application, with paypal payment gateway,
+        title: 'Shoppers World',
+        text: `An e-commerce shopping website is a responsive full stack web application, with paypal payment gateway,
          which includes both user and admin pannel, admin email Id =  admin@gmail.com, password = admin`
     },
     {
         id: 3,
         category: 'MERN',
-        image: project,
+        image: blog,
         link1: 'https://github.com/Dharanikumar19/Blog-Website-FrontEnd',
         link2: 'https://github.com/Dharanikumar19/Blog-Website-Backend',
         link3: 'https://dk-blog-website.netlify.app',
-        title: 'Blog Website',
-        text: `Blog Website is a responsive full stack application, made by MD Bootstarp with google login`
+        title: 'Bloggers Arena',
+        text: `Bloggers Arena is an responsive full stack blog application, made by MD Bootstarp with google login,
+               where user can create, read, update and delete blogs.`
     },
     {
         id: 4,
         category: 'MERN',
-        image: project,
+        image: social,
         link1: 'https://github.com/Dharanikumar19/Social-Media-Frontend',
         link2: 'https://github.com/Dharanikumar19/Social-Media-Backend',
         link3: 'https://dk-social-media-app.netlify.app',
-        title: 'Social Media Website',
-        text: `Social Media website (Facebook clone) is a full stack application, where user can create post,
+        title: 'Mingle Media',
+        text: `mingle media is an full stack social media application, where user can create post,
                follow and unfollow friends and also chat with their friends.`
     },
 ]
